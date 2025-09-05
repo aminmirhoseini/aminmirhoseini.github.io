3f279274bf5f63760f4adeec19d245374218b441
test <test@gmail.com>
2025-09-05 08:08:45 -0400

    add sliding words to main section

--- script.js
+++ script.js
@@ -141,3 +141,40 @@ function copyText() {
         })
         .catch(err => console.error("Copy failed:", err));
 }
+
+// word sliding
+let words = document.querySelectorAll(".word");
+words.forEach((word) => {
+    let letter = word.textContent.split("");
+    word.textContent = "";
+    letter.forEach((letter) => {
+        let span = document.createElement("span");
+        span.textContent = letter;
+        span.className = "letter"
+        word.append(span);
+    });
+});
+
+let currentWordIndex = 0;
+let maxWordIndex = words.length - 1;
+words[currentWordIndex].style.opacity = "1";
+let changeText =()=>{
+    let currentWord = words[currentWordIndex]
+    let nextWord = currentWordIndex === maxWordIndex ? words[0] : words[currentWordIndex + 1];
+
+    Array.from(currentWord.children).forEach((letter, i) => {
+        setTimeout(() => {
+            letter.className = "letter out";
+        }, i * 80);
+    });
+    nextWord.style.opacity = "1";
+    Array.from(nextWord.children).forEach((letter, i) => {
+        letter.className = "letter behind";
+        setTimeout(() => {
+            letter.className = "letter in";
+        }, 340 + i * 80);
+    });
+    currentWordIndex = currentWordIndex === maxWordIndex ? 0 : currentWordIndex + 1;
+};
+changeText();
+setInterval(changeText, 3000);
